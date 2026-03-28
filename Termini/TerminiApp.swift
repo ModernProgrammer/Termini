a0e1e8c007d839ccc5ed740e6fbfbc7d2125988c
Diego Bustamante <diegobust4545@icloud.com>
2026-03-28 18:14:37 -0400

    Initial Commit

---
+++ Termini/TerminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminiApp.swift
+//  Termini
+//
+//  Created by Diego on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TerminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
